6dbe6b5bc1366ae32165bbf8d143f199a9e49477
Javier <javier.su.weijie@gmail.com>
2015-04-09 19:56:04 +0800

    wip

--- fluid_simulation/main.cpp
+++ fluid_simulation/main.cpp
@@ -36,7 +36,7 @@ bool left_mouse_down;
 mouse mouse;
 vector<vec2> points = {};
 GLuint d0, d, d_fbo_A, d_fbo_B;
-GLuint diffuse_shader;
+GLuint diffuse_shader, display_shader;
 
 GLuint vao;
 
@@ -91,12 +91,55 @@ void idle_f(void) {
   glutPostRedisplay();
 }
 
-void draw_texture() {
+void update_diffuse() {
   glUseProgram(diffuse_shader);
-  glUniform1i(glGetUniformLocation(diffuse_shader, "Texture"),0);
-
-  glBindTexture(GL_TEXTURE_2D, d0);
+  glUniform1i(glGetUniformLocation(diffuse_shader, "Texture0"),0);
+  glUniform1i(glGetUniformLocation(diffuse_shader, "Texture1"),1);
+  glUniform1f(glGetUniformLocation(diffuse_shader, "pixelSize"), 1.0/width);
+  
+  glBindFramebuffer(GL_FRAMEBUFFER, d_fbo_A);
+  glViewport(0, 0, width, height);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
   glEnable(GL_TEXTURE_2D);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, d0);
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, d);
+  
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+  
+  glBindVertexArrayAPPLE(vao);
+  glDrawElements(GL_QUADS, 4, GL_UNSIGNED_INT, NULL);
+  glDisable(GL_TEXTURE_2D);
+}
+
+void swap_texture() {
+  glBindFramebuffer(GL_FRAMEBUFFER, d_fbo_A);
+  glUseProgram(display_shader);
+  glUniform1i(glGetUniformLocation(display_shader, "Texture0") ,0);
+  
+  glEnable(GL_TEXTURE_2D);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, d0);
+  
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+  
+  glBindVertexArrayAPPLE(vao);
+  glDrawElements(GL_QUADS, 4, GL_UNSIGNED_INT, NULL);
+  glDisable(GL_TEXTURE_2D);
+}
+
+void draw_texture() {
+  
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glUseProgram(display_shader);
+  glUniform1i(glGetUniformLocation(display_shader, "Texture0") ,0);
+  
+  glEnable(GL_TEXTURE_2D);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, d);
+
   glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
   
   glBindVertexArrayAPPLE(vao);
@@ -106,8 +149,11 @@ void draw_texture() {
 
 
 void draw_f(void) {
+  glClearColor(0.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  
+  swap_texture();
+//  for (int i = 0; i<20; i++)
+  update_diffuse();
   draw_texture();
   
   glutSwapBuffers();
@@ -124,40 +170,47 @@ void init_textures() {
   
   GLubyte* data = (GLubyte *) malloc(width*height*sizeof(GLubyte));
   int i = width/2;
-  data[index(i-1,i-1)] = 255;
-  data[index(i,i)] = 255;
-  data[index(i-3,i-3)] = 255;
+  for (int k=0;k<5;k++) {
+    data[index(i-k,i-k)] = 255;
+    data[index(i-k,i)] = 255;
+    data[index(i,i-k)] = 255;
+    data[index(i,i)] = 255;
+    data[index(i+k,i+k)] = 255;
+    data[index(i+k,i)] = 255;
+    data[index(i,i+k)] = 255;
+  }
   
   glGenTextures(1, &d0);
   glBindTexture(GL_TEXTURE_2D, d0);
-  glTexImage2D(GL_TEXTURE_2D, 0,GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+  glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, 512, 512, 0, GL_RED, GL_UNSIGNED_BYTE, data);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   
+  GLubyte* empty = (GLubyte *) malloc(width*height*sizeof(GLubyte));
   glGenTextures(1, &d);
   glBindTexture(GL_TEXTURE_2D, d);
-  glTexImage2D(GL_TEXTURE_2D, 0,GL_RED, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, 0);
+  glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, 512, 512, 0, GL_RED, GL_UNSIGNED_BYTE, empty);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   
-  glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void init_framebuffer() {
   // Create density framebuffers;
   glGenFramebuffers(1, &d_fbo_A);
   glBindFramebuffer(GL_FRAMEBUFFER, d_fbo_A);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,d, 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, d, 0);
   
   glGenFramebuffers(1, &d_fbo_B);
   glBindFramebuffer(GL_FRAMEBUFFER, d_fbo_B);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,d0, 0);
-  
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, d0, 0);
+  if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) exit(0);
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void init_shaders() {
   diffuse_shader = loadProgram("shaders/basic.vert", "shaders/diffuse.frag");
+  display_shader = loadProgram("shaders/basic.vert", "shaders/basic.frag");
 }
 
 /*
@@ -167,8 +220,19 @@ void init_shaders() {
  */
 
 void init_vao() {
-  float vertex[] = { -1.0f, 1.0f, .0f, -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 1.0f, 1.0f, 0.0f };
-  float texture[] = { 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0 };
+  float vertex[] = {
+    -1.0f, 1.0f, .0f,
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    1.0f, 1.0f, 0.0f
+  };
+  float texture[] = {
+    0.0, 1.0,
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0
+  };
+  
   unsigned int index[] = { 0, 1, 2, 3 };
   glGenVertexArraysAPPLE(1, &vao);
   glBindVertexArrayAPPLE(vao);
@@ -213,6 +277,7 @@ void init_glut() {
   glutCreateWindow("Fluid simulation");
   init_textures();
   init_shaders();
+  init_framebuffer();
   init_vao();
   glutReshapeFunc(reshape_f);
   glutKeyboardFunc(keyboard_f);

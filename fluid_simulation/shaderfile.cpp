4ffeab9b923176ef05a0270a13c9f267eb692bd5
Javier <javier.su.weijie@gmail.com>
2015-04-09 11:25:31 +0800

    add: debug information for shader compilation

--- fluid_simulation/shaderfile.cpp
+++ fluid_simulation/shaderfile.cpp
@@ -34,13 +34,29 @@ GLuint loadProgram(char* vert_filename, char* frag_filename)
     GLint length;
     char *log;
     
+    GLint frag_length;
+    char * frag_log;
+    
+    GLint vert_length;
+    char * vert_log;
+    
     /* get the program info log */
     glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
     log = (char*)malloc(length);
     glGetProgramInfoLog(program, length, &result, log);
     
+    glGetShaderiv(vs, GL_INFO_LOG_LENGTH, &vert_length);
+    vert_log = (char*)malloc(vert_length);
+    glGetShaderInfoLog(vs, vert_length, &result, vert_log);
+    
+    glGetShaderiv(fs, GL_INFO_LOG_LENGTH, &frag_length);
+    frag_log = (char*)malloc(frag_length);
+    glGetShaderInfoLog(fs, frag_length, &result, frag_log);
+    
     /* print an error message and the info log */
     fprintf(stderr, "sceneInit(): Program linking failed: %s\n", log);
+    fprintf(stderr, "sceneInit(): Vert linking failed: %s\n", vert_log);
+    fprintf(stderr, "sceneInit(): Frag linking failed: %s\n", frag_log);
     free(log);
     
     /* delete the program */
